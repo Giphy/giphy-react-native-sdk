ec18b1b35cd7d3084ed42de0cf7e5ac7d68b3429
Ivan Konstantynov <samlaughter@gmail.com>
2021-05-07 15:42:55 +0300

    upgrade kotlin version

--- android/src/main/java/com/giphyreactnativesdk/GiphyReactNativeSdkPackage.kt
+++ android/src/main/java/com/giphyreactnativesdk/GiphyReactNativeSdkPackage.kt
@@ -4,8 +4,6 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-import java.util.Collections.emptyList
-import com.giphyreactnativesdk.GiphyUIModule
 
 
 class GiphyReactNativeSdkPackage : ReactPackage {
