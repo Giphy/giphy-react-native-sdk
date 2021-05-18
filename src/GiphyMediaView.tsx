4c63d8a2a504b9ce0aabf6b075a4bca69c07371f
merlin <v.pshonyuk@gmail.com>
2021-05-18 17:23:02 +0300

    add support media configuration

--- src/GiphyMediaView.tsx
+++ src/GiphyMediaView.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import type { ViewStyle } from 'react-native';
+import React from 'react'
+import type { ViewStyle } from 'react-native'
 
-import { NativeGiphyMediaView, NativeGiphyMediaViewProps } from './native/GiphyMediaView';
+import { NativeGiphyMediaView, NativeGiphyMediaViewProps } from './native/GiphyMediaView'
 
-export type GiphyMediaViewProps = NativeGiphyMediaViewProps & { style?: ViewStyle };
+export type GiphyMediaViewProps = NativeGiphyMediaViewProps & { style?: ViewStyle }
 
 export class GiphyMediaView extends React.Component<GiphyMediaViewProps, {}> {
   render() {
-    return <NativeGiphyMediaView {...this.props} />;
+    return <NativeGiphyMediaView {...this.props} />
   }
 }
