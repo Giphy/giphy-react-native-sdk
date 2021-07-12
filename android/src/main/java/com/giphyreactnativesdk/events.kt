971a9274a1f25efe209551eb174331bb8d3aa909
Ivan Konstantynov <samlaughter@gmail.com>
2021-07-12 16:11:34 +0300

    cleanup

--- android/src/main/java/com/giphyreactnativesdk/events.kt
+++ android/src/main/java/com/giphyreactnativesdk/events.kt
@@ -3,7 +3,6 @@ package com.giphyreactnativesdk
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.WritableMap
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
-import com.facebook.react.uimanager.events.RCTEventEmitter
 
 
 enum class GiphyDialogEvents (val rnEvent: String) {
@@ -17,6 +16,3 @@ fun emitModuleEvent(reactContext: ReactContext, eventName: String, params: Writa
     .emit(eventName, params)
 }
 
-//fun emitViewEvent(reactContext: ReactContext, view: , eventName: String, params: WritableMap?) {
-//  reactContext.getJSModule(RCTEventEmitter::class.java).receiveEvent()
-//}
