fe2fe9f6a3f6bbeaed68b74ac2e211808e933381
Alex Lonsky <alexander.lonsky@gmail.com>
2026-07-02 12:48:04 +0300

    chore(example): migrate to react-native-safe-area-context build(deps): bump Android SDK version from 2.4.2 to 2.5.1

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { GiphyDialog, type GiphyDialogConfig, type GiphyMedia, GiphyVideoManager } from '@giphy/react-native-sdk'
 
 import './giphy.setup'
